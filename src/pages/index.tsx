0a0fb3f0c515ce7801eec8425d1f8e4e9d198a78
flaviobvds <40229603+flaviobvds@users.noreply.github.com>
2023-03-08 00:49:56 -0400

    applying first tailwind classes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import styles from '@/styles/Home.module.css'
 
 export default function Home() {
     return (
@@ -9,8 +8,14 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <main className={styles.main}>
-
+            <main className="bg-[url('/images/background.jpg')] h-screen bg-cover bg-no-repeat flex">
+                <div className="flex m-auto h-5/6 w-11/12 bg-white bg-opacity-20 shadow-2xl">
+                    <h1 className="font-sans text-5xl font-bold text-red-700">
+                        Get notified in every <br/>
+                        Express Entry <br/>
+                        draw
+                    </h1>
+                </div>
             </main>
         </>
     )
