1cb3062df7db1a1f658e2e4c881ad7139a67a2d2
flaviobvds <40229603+flaviobvds@users.noreply.github.com>
2023-03-09 01:47:20 -0400

    created form states

--- src/components/SignUpForm/index.tsx
+++ src/components/SignUpForm/index.tsx
@@ -1,15 +1,32 @@
+import { FormEvent, useState } from "react"
+
 export function SignUpForm() {
+
+    const [email, setEmail] = useState('')
+    const [agreeTC, setAgreeTC] = useState(false)
+
+    function handleSubmitForm(e: FormEvent) {
+        e.preventDefault();
+        console.log(email)
+        console.log(agreeTC)
+    }
+
     return (
-        <form className="flex flex-col mt-5 lg:justify-center lg:mt-0 sm:items-center">
+        <form
+            className="flex flex-col mt-5 lg:justify-center lg:mt-0 sm:items-center"
+            onSubmit={handleSubmitForm}
+        >
             <input
                 placeholder='E-mail'
                 type={'email'}
+                onChange={e => setEmail(e.target.value)}
                 className='p-3 focus:outline-none border-l-2 border-red-700 placeholder:text-black rounded 
                     ml-5 w-10/12 max-w-sm
                     sm:w-6/12
                     md:w-5/12 md:ml-0
                     lg:w-3/4 lg:max-w-none
-                    xl:w-7/12'
+                    xl:w-7/12
+                '
             />
 
             <div className='flex h-14 items-center mt-4 ml-5 w-11/12 
@@ -21,6 +38,7 @@ export function SignUpForm() {
                 <input
                     type={'checkbox'}
                     className='accent-red-700 cursor-pointer h-4 w-4'
+                    onChange={e => setAgreeTC(e.target.checked)}
                 />
                 <span className='inline-block my-auto ml-3'>
                     I agree with the
