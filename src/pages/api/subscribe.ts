c6c09124a7a9407cf7ed2cebe3365c6fdd598643
flaviobvds <40229603+flaviobvds@users.noreply.github.com>
2023-03-11 01:47:05 -0400

    mongodb database connection

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,11 +1,33 @@
-import mongodb from 'mongodb'
+import {MongoClient, Db} from 'mongodb'
 import { NextApiRequest, NextApiResponse } from "next";
+
+let cachedDb: Db | null = null;
+
+async function connectToDatabase(uri: string) {
+    if (cachedDb) {
+        return cachedDb
+    }
+
+    const client = await MongoClient.connect(uri)
+    const db = client.db('eedrawsUsers')
+    cachedDb = db;
+    return db;
+}
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     if (req.method === 'POST') {
         const email: string = req.body.email
 
-        
+        const db = await connectToDatabase(process.env.MONGODB_URI!)
+        const collection = db.collection('subscribers')
+
+        await collection.insertOne({
+            email,
+            subscribedAt: new Date()
+        })
+
+        return res.status(201).json({ok: true})
+
     } else {
         res.setHeader('Allow', 'POST')
         res.status(405).end('Method not Allowed')
