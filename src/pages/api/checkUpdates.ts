deac098ead990dcfd684d47721671f3538eccd9e
flaviobvds <40229603+flaviobvds@users.noreply.github.com>
2023-03-15 15:47:54 -0400

    send email on Updates workflow implemented

--- src/pages/api/checkUpdates.ts
+++ src/pages/api/checkUpdates.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { MongoClient, Db } from 'mongodb'
-import axios from "axios";
+import { MongoClient, Db, ObjectId } from 'mongodb'
+import sgMail from '@sendgrid/mail'
 import nodefetch from 'node-fetch'
 
 let cachedDb: Db | null = null;
@@ -45,14 +45,58 @@ interface JSONResponse {
 }
 
 async function connectToDatabase(uri: string) {
+    
     if (cachedDb) {
         return cachedDb
     }
-
+    
     const client = await MongoClient.connect(uri)
     const db = client.db('eedraws')
     cachedDb = db;
     return db;
+}
+
+function html(unsubscribeLink: string) {
+    const html = 
+    `<table style="border-collapse: collapse; width: 100%; max-width: 450px; margin-left: auto; margin-right: auto;">
+        <tbody>
+            <tr style="height: 18px; background-color: #b91c1c;">
+                <td style="width: 100%; height: 18px; text-align: center;">
+                    <h1><span style="color: #ffffff;">Welcome!</span></h1>
+                </td>
+            </tr>
+            <tr style="height: 80px;">
+                <td style="width: 100%; height: 120px;">
+                    <p style="text-align: center;">You have successfully signed up to EE Draws Notifier and, from now on, you won't miss any new Express Entry draw. And the best of all: <span style="color: #b91c1c;"><strong>it's free!</strong></span></p>
+                    <p style="text-align: center;">&nbsp;</p>
+                    <p style="text-align: center;"><span><a style="color: #b91c1c;" href="${unsubscribeLink}">Unsubscribe</a></span></p>
+                </td>
+            </tr>
+        </tbody>
+    </table>`
+    return html
+}
+
+async function sendGridMail(email: string, unsubscribeId: string) {
+    const unsubscribeLink = 'https://eedraws.online/unsubscribe/' + unsubscribeId
+    
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY!)
+
+    const msg = {
+        to: email,
+        from: 'EE Draws Team <donotreply@eedraws.online>',
+        subject: 'New Express Entry Draw',
+        text: "IRCC just released a new Express Entry Draw!",
+        html: html(unsubscribeLink)
+    }
+
+    await sgMail.send(msg).then(() => {
+        console.log('email sent')
+    }).catch((error) => {
+        console.log(error)
+    })
+
+
 }
 
 const url = 'https://www.canada.ca/content/dam/ircc/documents/json/ee_rounds_123_en.json'
@@ -67,12 +111,15 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     const thisDraw = json.rounds[0]
 
     const db = await connectToDatabase(process.env.MONGODB_URI!)
-    const collection = db.collection('lastDraws')
+    const lastDrawsCollection = db.collection('lastDraws')
+    const subscribersCollection = db.collection('subscribers')
 
-    const recentDraw = await collection.find({}).sort({ createdAt: -1 }).next();
+    const recentDraw = await lastDrawsCollection.find({}).sort({ createdAt: -1 }).next();
 
     if (thisDraw.drawNumber != recentDraw?.drawNumber) {
-        await collection.insertOne({
+        
+        // insert newDraw on lastDrawsCollection
+        await lastDrawsCollection.insertOne({
             drawNumber: thisDraw.drawNumber,
             drawDate: thisDraw.drawDate,
             drawName: thisDraw.drawName,
@@ -80,6 +127,11 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
             drawSize: thisDraw.drawSize,
             createdAt: new Date()
         })
+
+        // get emails from subscribersCollection
+        const allUsers = await subscribersCollection.find().map(doc => doc).toArray()
+        allUsers.forEach((user) => sendGridMail(user.email, user._id.toString()))
+        
         return (res.send('new draw'))
     }
 
