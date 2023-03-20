5ef665521360ef8ed35b8aee225484d5d5250d95
flaviobvds <40229603+flaviobvds@users.noreply.github.com>
2023-03-19 20:24:13 -0400

    screenshots

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ export default function Home() {
             <main className="bg-[url('/images/background.jpg')] h-screen bg-cover bg-no-repeat flex relative">
 
                 <div className="grid grid-rows-2
-                    m-auto h-3/4 w-11/12 max-w-screen-xl bg-white bg-opacity-20 shadow-2xl backdrop-blur rounded
+                    m-auto h-5/6 w-11/12 max-w-screen-xl bg-white bg-opacity-20 shadow-2xl backdrop-blur rounded
                     sm:grid-cols-2 sm:grid-rows-none"
                 >
                     <TitleSection />
