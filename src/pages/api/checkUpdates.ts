3d8fd1364a7969dc9bb8e0de889bf7aada14f989
Flavio Silva <flaviobvds@gmail.com>
2023-12-06 13:06:41 -0700

    Update checkUpdates.ts

--- src/pages/api/checkUpdates.ts
+++ src/pages/api/checkUpdates.ts
@@ -89,7 +89,6 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     const json: JSONResponse = (await data.json()) as any;
     const thisDraw = json.rounds[0]
     console.log(thisDraw)
-    console.log(json)
 
     const db = await connectToDatabase(process.env.MONGODB_URI!)
     const lastDrawsCollection = db.collection('lastDraws')
